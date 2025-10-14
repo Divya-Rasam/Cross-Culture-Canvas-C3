f5f44c67f0660cf0d65e665960063ab4c3b9e21f
Divya-Rasam <divyaarunrasam@gmail.com>
2025-10-14 16:25:05 +0530

    space_repository

--- src/main/java/com/underground/c3_backend/repositories/SpaceRepository.java
+++ src/main/java/com/underground/c3_backend/repositories/SpaceRepository.java
@@ -1,14 +1,13 @@
 package com.underground.c3_backend.repositories;
 
-import java.util.List;
-
+import com.underground.c3_backend.entities.SpaceStatus;
+import com.underground.c3_backend.entities.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.underground.c3_backend.entities.Space;
-import com.underground.c3_backend.entities.SpaceStatus;
+import java.util.List;
 
 @Repository
 public interface SpaceRepository extends JpaRepository<Space, Long> {
@@ -17,5 +16,47 @@ public interface SpaceRepository extends JpaRepository<Space, Long> {
     @Query("SELECT s FROM Space s WHERE s.status = :status")
     List<Space> findApprovedSpaces(@Param("status") SpaceStatus status);
     
-    List<Space> findBySubmittedByUserId(Long userId);
+    @Query("SELECT s FROM Space s WHERE s.submittedBy.id = :userId")
+    List<Space> findBySubmittedByUserId(@Param("userId") Long userId);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package com.underground.c3_backend.repositories;
+
+// import java.util.List;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
+// import org.springframework.stereotype.Repository;
+
+// import com.underground.c3_backend.entities.Space;
+// import com.underground.c3_backend.entities.SpaceStatus;
+
+// @Repository
+// public interface SpaceRepository extends JpaRepository<Space, Long> {
+//     List<Space> findByStatus(SpaceStatus status);
+    
+//     @Query("SELECT s FROM Space s WHERE s.status = :status")
+//     List<Space> findApprovedSpaces(@Param("status") SpaceStatus status);
+    
+//     List<Space> findBySubmittedByUserId(Long userId);
+// }
--- src/main/java/com/underground/c3_backend/repositories/UserRepository.java
+++ src/main/java/com/underground/c3_backend/repositories/UserRepository.java
@@ -1,11 +1,10 @@
 package com.underground.c3_backend.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.underground.c3_backend.entities.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,3 +13,74 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package com.underground.c3_backend.repositories;
+
+// import java.util.Optional;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import com.underground.c3_backend.entities.User;
+
+// @Repository
+// public interface UserRepository extends JpaRepository<User, Long> {
+//     Optional<User> findByUsername(String username);
+//     Optional<User> findByEmail(String email);
+//     boolean existsByUsername(String username);
+//     boolean existsByEmail(String email);
+// }
